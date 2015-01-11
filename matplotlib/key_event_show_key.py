e721dd5a52a5727f0da092b2e7abd93a29165c08
ruoyu0088 <ruoyu0088@gmail.com>
2015-01-11 13:46:10 +0900

    show key event in figure

--- matplotlib/key_event_show_key.py
+++ matplotlib/key_event_show_key.py
@@ -2,11 +2,11 @@ import sys
 from matplotlib import pyplot as plt
 
 fig, ax = plt.subplots()
-
+text = ax.text(0.5, 0.5, "event", ha="center", va="center", fontdict={"size": 20})
 
 def on_key_press(event):
-    print event.key
-    sys.stdout.flush()
+    text.set_text(event.key)
+    fig.canvas.draw_idle()
 
 
 fig.canvas.mpl_connect('key_press_event', on_key_press)
