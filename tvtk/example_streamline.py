c03397216fe532b828a936ff2fcc9ac1b595f737
ruoyu0088 <ruoyu0088@gmail.com>
2015-03-07 12:41:07 +0900

    update for VTK 6.0

--- tvtk/example_streamline.py
+++ tvtk/example_streamline.py
@@ -2,62 +2,71 @@
 from example_cut_plane import read_data
 import numpy as np
 from tvtk.api import tvtk
+from tvtk.common import configure_input, configure_source_data
 
-###1###
-# 读入数据
 plot3d = read_data()
+grid = plot3d.output.get_block(0)
 
-# 矢量箭头
-mask = tvtk.MaskPoints(input = plot3d.output, random_mode=True,
-                        on_ratio=50, maximum_number_of_points=10000) #{1}
-arrow_source = tvtk.ArrowSource() #{2}
-arrows = tvtk.Glyph3D(input = mask.output, source=arrow_source.output, #{3}
-    scale_factor=2/np.max(plot3d.output.point_data.scalars.to_array())) 
-arrows_mapper = tvtk.PolyDataMapper(input = arrows.output, 
-    scalar_range = plot3d.output.point_data.scalars.range)
-arrows_actor = tvtk.Actor(mapper = arrows_mapper)
-###1###
-###2###
-# 作为流线起点的球
-center = plot3d.output.center
-sphere = tvtk.SphereSource(  #{1}
-    center=(2, center[1], center[2]), radius=2, 
+mask = tvtk.MaskPoints(random_mode=True,
+                       on_ratio=50, maximum_number_of_points=1000)
+configure_input(mask, grid)
+
+arrow_source = tvtk.ArrowSource()
+arrows = tvtk.Glyph3D()
+arrows.set_source_connection(arrow_source.output_port)
+configure_input(arrows, mask)
+
+arrows_mapper = tvtk.PolyDataMapper(scalar_range=grid.point_data.scalars.range)
+configure_input(arrows_mapper, arrows)
+
+arrows_actor = tvtk.Actor(mapper=arrows_mapper)
+
+center = grid.center
+sphere = tvtk.SphereSource(
+    center=(2, center[1], center[2]), radius=2,
     phi_resolution=6, theta_resolution=6)
-sphere_mapper = tvtk.PolyDataMapper(input = sphere.output)
-sphere_actor = tvtk.Actor(mapper = sphere_mapper)
-sphere_actor.property.set(
-    representation = "wireframe", color=(0,0,0))
+sphere_mapper = tvtk.PolyDataMapper()
+configure_input(sphere_mapper, sphere)
 
-# 流线    
-streamer = tvtk.StreamLine( #{2}
-    input = plot3d.output,
-    source = sphere.output,
-    step_length = 0.0001,
-    integration_direction = "forward", 
-    integrator = tvtk.RungeKutta4()) #{3}
-    
-tube = tvtk.TubeFilter( #{4}
-    input = streamer.output,
-    radius = 0.05,
-    number_of_sides = 6,
-    vary_radius = "vary_radius_by_scalar")
+sphere_actor = tvtk.Actor(mapper=sphere_mapper)
+sphere_actor.property.set(
+    representation="wireframe", color=(0, 0, 0))
+
+streamer = tvtk.StreamLine(
+    step_length=0.0001,
+    integration_direction="forward",
+    integrator=tvtk.RungeKutta4())
+configure_input(streamer, grid)
+configure_source_data(streamer, sphere.output)
+
+tube = tvtk.TubeFilter(
+    radius=0.05,
+    number_of_sides=6,
+    vary_radius="vary_radius_by_scalar")
+configure_input(tube, streamer)
 
 tube_mapper = tvtk.PolyDataMapper(
-    input = tube.output,
-    scalar_range = plot3d.output.point_data.scalars.range)
-tube_actor = tvtk.Actor(mapper = tube_mapper)  
+    scalar_range=grid.point_data.scalars.range)
+configure_input(tube_mapper, tube)
+
+tube_actor = tvtk.Actor(mapper=tube_mapper)
 tube_actor.property.backface_culling = True
-###2###
-# StructuredGrid网格的外框
-outline = tvtk.StructuredGridOutlineFilter(input = plot3d.output)
-outline_mapper = tvtk.PolyDataMapper(input = outline.output)
-outline_actor = tvtk.Actor(mapper = outline_mapper)
+
+outline = tvtk.StructuredGridOutlineFilter()
+configure_input(outline, grid)
+
+outline_mapper = tvtk.PolyDataMapper()
+configure_input(outline_mapper, outline)
+
+outline_actor = tvtk.Actor(mapper=outline_mapper)
 outline_actor.property.color = 0.3, 0.3, 0.3
 
 from scpy2.tvtk.tvtkhelp import ivtk_scene
+
 win = ivtk_scene([outline_actor, sphere_actor, tube_actor, arrows_actor])
 win.scene.isometric_view()
 
 from pyface.api import GUI
+
 gui = GUI()
 gui.start_event_loop()
