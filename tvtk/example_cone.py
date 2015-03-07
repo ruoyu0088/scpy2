721816b8dfddc36ebcde54a1654139bb632a9c2c
ruoyu0088 <ruoyu0088@gmail.com>
2015-03-07 11:43:05 +0900

    add example_cone

---
+++ tvtk/example_cone.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from tvtk.api import tvtk
+from tvtk.common import configure_input
+
+# 创建一个圆锥数据源，并且同时设置其高度，底面半径和底面圆的分辨率(用36边形近似)
+cs = tvtk.ConeSource(height=3.0, radius=1.0, resolution=36)
+# 使用PolyDataMapper将数据转换为图形数据
+m = tvtk.PolyDataMapper()
+configure_input(m, cs)
+# 创建一个Actor
+a = tvtk.Actor(mapper=m)
+# 创建一个Renderer，将Actor添加进去
+ren = tvtk.Renderer(background=(1, 1, 1))
+ren.add_actor(a)
+# 创建一个RenderWindow(窗口)，将Renderer添加进去
+rw = tvtk.RenderWindow(size=(300,300))
+rw.add_renderer(ren)
+# 创建一个RenderWindowInteractor（窗口的交互工具)
+rwi = tvtk.RenderWindowInteractor(render_window=rw)
+# 开启交互
+rwi.initialize()
+rwi.start()
