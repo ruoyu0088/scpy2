f1c1c20b91c959115778f596f95f6b32cefff5dc
ruoyu0088 <ruoyu0088@gmail.com>
2015-03-15 13:17:22 +0900

    update to VTK 6

--- tvtk/tvtkhelp.py
+++ tvtk/tvtkhelp.py
@@ -11,7 +11,7 @@ def depth_peeling(scene):
     renderer.occlusion_ratio = 0.1
 
 
-def vtk_scene_to_array(scene):
+def vtk_scene_to_array(scene, close=True):
     import numpy as np
     from tvtk.api import tvtk
 
@@ -32,6 +32,8 @@ def vtk_scene_to_array(scene):
     mask = np.all(arr == arr[0, 0], -1)
     x0, x1 = np.where(~np.all(mask, axis=0))[0][[0, -1]]
     y0, y1 = np.where(~np.all(mask, axis=1))[0][[0, -1]]
+    if close:
+        scene.close()
     return arr[y0:y1 + 1, x0:x1 + 1, :].copy()
 
 
@@ -43,28 +45,32 @@ def vtk_convexhull(ch, radius=0.02):
     poly.polys = ch.simplices
 
     sphere = tvtk.SphereSource(radius=radius)
-    points3d = tvtk.Glyph3D(input=poly, source=sphere.output)
+    points3d = tvtk.Glyph3D()
+    points3d.set_source_connection(sphere.output_port)
+    points3d.set_input_data(poly)
 
-    m1 = tvtk.PolyDataMapper(input=poly)
+    m1 = tvtk.PolyDataMapper()
+    m1.set_input_data(poly)
     a1 = tvtk.Actor(mapper=m1)
     a1.property.opacity = 0.3
 
-    m2 = tvtk.PolyDataMapper(input=poly)
+    m2 = tvtk.PolyDataMapper()
+    m2.set_input_data(poly)
     a2 = tvtk.Actor(mapper=m2)
     a2.property.representation = "wireframe"
     a2.property.line_width = 2.0
     a2.property.color = (1.0, 0, 0)
 
-    m3 = tvtk.PolyDataMapper(input=points3d.output)
+    m3 = tvtk.PolyDataMapper(input_connection=points3d.output_port)
     a3 = tvtk.Actor(mapper=m3)
     a3.property.color = (0.0, 1.0, 0.0)
-    return a1, a2, a3
+    return [a1, a2, a3]
 
 
 def vtk_scene(actors, size=(800, 600), azimuth=None, elevation=None, viewangle=None):
     from tvtk.pyface.tvtk_scene import TVTKScene
 
-    scene = TVTKScene(off_screen_rendering=True)
+    scene = TVTKScene(off_screen_rendering=False)
     scene._renwin.size = size
     scene.add_actors(actors)
     if azimuth is not None:
