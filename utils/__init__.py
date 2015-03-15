e1a1b587736a352ca11a472f557969ee421257eb
ruoyu0088 <ruoyu0088@gmail.com>
2015-03-15 10:07:01 +0900

    add tabulate_it

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 from .select_compiler import show_compiler, set_msc_version, set_compiler
+
+
+def tabulate_it(rows, cols, func, label_fmt="**{}**"):
+    from tabulate import tabulate
+    result = []
+
+    fmt = label_fmt.format
+
+    for row in rows:
+        res_row = [fmt(row)]
+        result.append(res_row)
+        for col in cols:
+            res_row.append(func(row, col))
+    return tabulate(result, [fmt(col) for col in cols], "pipe")
