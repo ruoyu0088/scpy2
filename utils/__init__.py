f681be616ed28a0d6fc7eda98654e108f2213db2
ruoyu0088 <ruoyu0088@gmail.com>
2015-09-12 18:30:11 +0900

    add row_label argument

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,14 +2,14 @@
 from .select_compiler import show_compiler, set_msvc_version, set_compiler
 
 
-def tabulate_it(rows, cols, func, label_fmt="**{}**"):
+def tabulate_it(rows, cols, func, label_fmt=u"**{}**", row_label=True):
     from tabulate import tabulate
     result = []
 
     fmt = label_fmt.format
 
     for row in rows:
-        res_row = [fmt(row)]
+        res_row = [fmt(row)] if row_label else []
         result.append(res_row)
         for col in cols:
             res_row.append(func(row, col))
